02bcef35ad42e65b76ba7f0513dd5771e3e35aa8
Raymond Quinitero A <rquintero@gr_comeca.co.cr>
2019-01-27 21:39:43 -0600

    Input, output y viewchild

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+
+import { FormsModule } from '@angular/forms';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+import { PagesComponent } from './pages.component';
+import { SharedModule } from '../shared/shared.module';
+import { PAGES_ROUTES } from './pages.route';
+
+// Plugins
+import { ChartsModule } from 'ng2-charts';
+
+// Temporales
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
+import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
+
+@NgModule({
+    declarations: [
+        PagesComponent,
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component,
+        IncrementadorComponent,
+        GraficoDonaComponent
+    ],
+    exports: [
+        PagesComponent,
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component
+    ],
+    imports: [
+        SharedModule,
+        PAGES_ROUTES,
+        FormsModule,
+        ChartsModule
+    ]
+})
+
+export class PagesModule { }
---
+++ src/app/pages/pages.route.ts
@@ -0,0 +1,18 @@
+import { RouterModule, Routes, Router } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+import { PagesComponent } from './pages.component';
+
+const APP_ROUTES: Routes = [
+    {
+        path: '', component: PagesComponent,
+        children: [
+            { path: 'dashboard', component: DashboardComponent },
+            { path: 'progress', component: ProgressComponent },
+            { path: 'graficas1', component: Graficas1Component },
+            { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
+    ] }
+];
+
+export const PAGES_ROUTES = RouterModule.forChild( APP_ROUTES );
