a3b388b487c8f0d51d7d0920c5298da64da07a25
Raymond <quinteroaparicio@gmail.com>
2024-06-12 19:44:41 -0600

    Actualizaciones correctas

--- src/app/services/service.module.ts
+++ src/app/services/service.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { SettingsService, SidebarService, SharedService, UsuarioService,
           SubirArchivoService, MedicoService } from './service.index';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 import { ModaluploadService } from '../components/modal-upload/modalupload.service';
 import { HospitalService } from './hospital/hospital.service';
@@ -13,24 +13,18 @@ import { LoginGuardGuard } from './guards/login-guard.guard';
 import { AdminGuard } from './guards/admin.guard';
 import { VerificaTokenGuard } from './guards/verifica-token.guard';
 
-@NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    HttpClientModule
-  ],
-  providers: [
-    SettingsService,
-    SidebarService,
-    SharedService,
-    UsuarioService,
-    SubirArchivoService,
-    ModaluploadService,
-    HospitalService,
-    MedicoService,
-    LoginGuardGuard,
-    AdminGuard,
-    VerificaTokenGuard
-  ]
-})
+@NgModule({ declarations: [], imports: [CommonModule], providers: [
+        SettingsService,
+        SidebarService,
+        SharedService,
+        UsuarioService,
+        SubirArchivoService,
+        ModaluploadService,
+        HospitalService,
+        MedicoService,
+        LoginGuardGuard,
+        AdminGuard,
+        VerificaTokenGuard,
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class ServiceModule { }
